57ac8ee7dd3edb492b9e49447c810a8a3e22159d
Brayan <brayantoby23@gmail.com>
2025-12-09 17:13:55 -0300

    tela de entrada criada

--- src/main/java/br/batpark/sp/jandira/estacionamento/ui/TelaInicialApp.java
+++ src/main/java/br/batpark/sp/jandira/estacionamento/ui/TelaInicialApp.java
@@ -42,6 +42,8 @@ public class TelaInicialApp extends Application {
         Button btnEntrada = new Button("Registrar Nova Entrada");
         Button btnSaida = new Button("Registrar Saída / Pagamento");
 
+        btnEntrada.setOnAction(e -> root.setCenter(criarTelaEntrada()));
+
         HBox boxBotoes = new HBox(30, btnEntrada, btnSaida);
         boxBotoes.setAlignment(Pos.CENTER);
 
@@ -53,6 +55,46 @@ public class TelaInicialApp extends Application {
 
         return dashboardLayout;
     }
+    private VBox criarTelaEntrada() {
+        Label titulo = new Label("Registrar entrada");
+        titulo.setFont(new Font("Adamina", 18));
 
+        GridPane formLayout = new GridPane();
+        formLayout.setHgap(30);
+        formLayout.setVgap(15);
+        formLayout.setPadding(new Insets(80));
+        formLayout.setAlignment(Pos.CENTER);
+
+        textFieldPlaca = new TextField();
+        textFieldProprietario = new TextField();
+        textFieldModelo = new TextField();
+
+        textFieldPlaca.setPrefWidth(300);
+
+
+
+        formLayout.addRow(0, new Label("Placa:"), textFieldPlaca);
+        formLayout.addRow(1, new Label("Proprietário:"), textFieldProprietario);
+        formLayout.addRow(2, new Label("Modelo:"), textFieldModelo);
+
+        Button btnConfirmar = new Button("Confirmar Entrada");
+
+        btnConfirmar.setOnAction(e -> {
+            // validação e CSV é aqui.
+            root.setCenter(criarTelaPrincipal());
+        });
+
+        Button btnVoltar = new Button("Voltar");
+        btnVoltar.setOnAction(e -> root.setCenter(criarTelaPrincipal()));
+
+        HBox boxBotoes = new HBox(20, btnVoltar, btnConfirmar);
+        boxBotoes.setAlignment(Pos.CENTER_RIGHT);
+
+        VBox entradaLayout = new VBox(20, titulo, formLayout, boxBotoes);
+        entradaLayout.setPadding(new Insets(30));
+        entradaLayout.setAlignment(Pos.TOP_CENTER);
+
+        return entradaLayout;
+    }
 
 }
