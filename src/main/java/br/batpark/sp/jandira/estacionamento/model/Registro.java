aceeebb9ebf7b3be34377f5d90f908981d2cb2d1
ARTHUR LIMA TRAUTWEIN <25203614@CFP127ALUNO.LOCAL>
2025-12-09 08:55:03 -0300

    Criação da estrutura básica

---
+++ src/main/java/br/batpark/sp/jandira/estacionamento/model/Registro.java
@@ -0,0 +1,9 @@
+package br.batpark.sp.jandira.estacionamento.model;
+
+public class Registro {
+    public String placa;
+    public String proprietario;
+    public String modelo;
+    public String data;
+    public String hora;
+}
---
+++ src/main/java/br/batpark/sp/jandira/estacionamento/ui/repository/Cadastro.java
@@ -0,0 +1,8 @@
+package br.batpark.sp.jandira.estacionamento.ui.repository;
+
+import java.nio.file.Path;
+
+public class Cadastro {
+    public Cadastro cadastro;
+    private Path arquivo = Paths.get("/Users/25203614/arquivos/veiculos_estacionados.csv");
+}
