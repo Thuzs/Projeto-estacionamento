e5a834d2885d9eacb8408c7be706cef45f54c9c1
Brayan <brayantoby23@gmail.com>
2025-12-09 15:58:00 -0300

    atualizando tela principal

--- src/main/java/br/batpark/sp/jandira/estacionamento/ui/TelaInicialApp.java
+++ src/main/java/br/batpark/sp/jandira/estacionamento/ui/TelaInicialApp.java
@@ -1,33 +1,58 @@
 package br.batpark.sp.jandira.estacionamento.ui;
 
-
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class TelaInicialApp extends Application {
+
     TextField textFieldPlaca;
     TextField textFieldProprietario;
     TextField textFieldModelo;
-    ListView veiculosEstacionados = new ListView();
+    TableView<String> veiculosEstacionados = new TableView<>();
+
+
+    BorderPane root = new BorderPane();
 
     @Override
-    public void start(Stage stage) throws IOException{
-        stage.setFullScreen(true);
+    public void start(Stage stage) throws IOException {
+        stage.setTitle("BAT Park - Gestão de Estacionamento");
+        stage.setMinWidth(1000);
+        stage.setMinHeight(600);
 
-        VBox root = new VBox();
+        root.setCenter(criarTelaPrincipal());
+
         Scene scene = new Scene(root);
-
         stage.setScene(scene);
-        stage.setTitle("Estacionamento");
         stage.show();
     }
 
+
+    private VBox criarTelaPrincipal() {
+
+        veiculosEstacionados.setPrefHeight(400);
+
+        Button btnEntrada = new Button("Registrar Nova Entrada");
+        Button btnSaida = new Button("Registrar Saída / Pagamento");
+
+        HBox boxBotoes = new HBox(30, btnEntrada, btnSaida);
+        boxBotoes.setAlignment(Pos.CENTER);
+
+
+        VBox dashboardLayout = new VBox(20, veiculosEstacionados,  boxBotoes);
+        dashboardLayout.setPadding(new Insets(30));
+        dashboardLayout.setAlignment(Pos.CENTER);
+
+
+        return dashboardLayout;
+    }
 
 
 }
